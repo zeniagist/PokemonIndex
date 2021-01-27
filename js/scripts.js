36a720f34bf4617ca114e3341962dd783c13d583
Zenia Gist <zeniagist@gmail.com>
2021-01-27 03:25:56 +0000

    wrapped script in pokemonRepository IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-// declare variables
-let pokemonList = [
-  { name: 'Jigglypuff', height: 0.5, types: ['Fairy'] },
-  { name: 'Eevee', height: 0.3, types: ['Field'] },
-  { name: 'Snorlax', height: 2.1, types: ['Monster'] }
-];
+let pokemonRepository = (function (){
+  // declare variables
+  let pokemonList = [
+    { name: 'Jigglypuff', height: 0.5, types: ['Fairy'] },
+    { name: 'Eevee', height: 0.3, types: ['Field'] },
+    { name: 'Snorlax', height: 2.1, types: ['Monster'] }
+  ];
 
-pokemonList.forEach(function(pokemon){
-  // print name and height
-  document.write(pokemon.name + " (height: " + pokemon.height + ")");
-  // print message if height is past threshold
-  if(pokemon.height >= 1){
-    document.write(" - Wow, that's big!");
-  }
-  document.write("<br>");
-});
+  pokemonList.forEach(function(pokemon){
+    // print name and height
+    document.write(pokemon.name + " (height: " + pokemon.height + ")");
+    // print message if height is past threshold
+    if(pokemon.height >= 1){
+      document.write(" - Wow, that's big!");
+    }
+    document.write("<br>");
+  });
+}());
