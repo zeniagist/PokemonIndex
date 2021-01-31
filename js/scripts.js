6dbdc840d88c149c7e4217f28c97addc33fbb430
Zenia Gist <zeniagist@gmail.com>
2021-01-31 16:35:42 +0000

    separated functions and return in js

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,31 @@ let pokemonRepository = (function (){
     { name: 'Jigglypuff', height: 0.5, types: ['Fairy'] },
     { name: 'Eevee', height: 0.3, types: ['Field'] },
     { name: 'Snorlax', height: 2.1, types: ['Monster'] }
-  ];   
+  ];
 
-  return{
-    // add pokemon to array
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    },
-    // returns pokemon list
-    getAll: function() {
-      pokemonList.forEach(function(pokemon){
-        // print name and height
-        document.write(pokemon.name + " (height: " + pokemon.height + ")");
-        // print message if height is past threshold
-        if(pokemon.height >= 1){
-          document.write(" - Wow, that's big!");
-        }
-        document.write("<br>");
-      });
-    }
-  }  
+  // Add pokemon to array
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  // returns pokemon list
+  function getAll() {
+    pokemonList.forEach(function(pokemon){
+      // print name and height
+      document.write(pokemon.name + " (height: " + pokemon.height + ")");
+      // print message if height is past threshold
+      if(pokemon.height >= 1){
+        document.write(" - Wow, that's big!");
+      }
+      document.write("<br>");
+    });
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };  
+
 }());
 
 // add pokemon
