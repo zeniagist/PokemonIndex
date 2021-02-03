5a0edc93323ab7e1233c5c573254ba0ac6b1f35a
Zenia Gist <zeniagist@gmail.com>
2021-02-03 04:16:12 +0000

    updated item image to sprite image

--- js/scripts.js
+++ js/scripts.js
@@ -57,7 +57,7 @@ let pokemonRepository = (function (){
       return response.json();
     }).then(function (details) {
       // Now we add the details to the item
-      item.imageUrl = details.sprites.front_default;
+      item.imageUrl = details.sprites.other.dream_world.front_default;
       item.height = details.height;
       item.types = [];
       details.types.forEach(function (pokemonType) {
